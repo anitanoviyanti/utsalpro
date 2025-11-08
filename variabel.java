3e58fab72fb76e829ef97c3967ecc1c7172d7987
anitanoviyanti <anitanoviyanti44@gmail.com>
2025-11-08 16:42:13 +0700

    UTS ALGORITMA DAN PEMROGRAMAN
    Bahasa Pemrograman Java

---
+++ variabel.java
@@ -0,0 +1,18 @@
+public class variabel {
+    public static void main(String[] args) {
+        String namaLengkap = "Anita Noviyanti";
+        String tempatTanggalLahir = "Bogor, 31 Oktober 2006";
+        String jenisKelamin = "Perempuan";
+        String alamatLengkap = "Rawa Ilat, Cileungsi, Bogor";
+        String noHP = "100020003000";
+        String penghasilanSebulan = "Rp 5.000.000";
+
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap            = " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir  = " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin           = " + jenisKelamin);
+        System.out.println("Nomor HP                = " + noHP);
+        System.out.println("Penghasilan Sebulan     = " + penghasilanSebulan);
+    }
+}
